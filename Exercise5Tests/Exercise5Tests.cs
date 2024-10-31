5fc96623395b7770eada20a3af79fe3b55f1fd09
sshabalala023 <sshabalala023@student.wethinkcode.co.za>
2024-10-31 11:51:10 +0200

    Exercise 5 Tests

--- Exercise5Tests/Exercise5Tests.cs
+++ Exercise5Tests/Exercise5Tests.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using Calculators;
 
 namespace Exercise5Tests;
@@ -10,20 +10,37 @@ public class Exercise5Tests
 
     [QaVerification.Grading]
     // use the [TestCase] annotation here
+    [TestCase(5.0, 7.0, 12.0)]
+    [TestCase(2.0,-3.0,-1)]
     public void ValuesBasedDataDrivenAddition(decimal value1, decimal value2, decimal expected)
     {
         // complete this method
-    }
 
+        // Act
+    
+        var result = Calculator.Enter(value1).Plus(value2).Equals();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected)); 
+
+
+    }
+    
     private static IEnumerable AdditionTestData()
     {
-        return null; // replace this line with your code
+        yield return new TestCaseData(1m, 1m, 2m);
+        yield return new TestCaseData(1m, 2m, 3m);
+        yield return new TestCaseData(-1m, -1m, -2m);
     }
 
     [QaVerification.Grading]
-    // use the [TestCaseSource] annotation here
+    [TestCaseSource(nameof(AdditionTestData))]
     public void GeneratorDataDrivenAddition(decimal value1, decimal value2, decimal expected)
     {
-        // complete this method
+        // Act
+        decimal result = Calculator.Reset().Enter(value1).Plus(value2).Equals();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
