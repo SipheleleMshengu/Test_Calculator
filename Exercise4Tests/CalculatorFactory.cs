dbee9b74efaabe7e588e2f32a39988aba37d8deb
sshabalala023 <sshabalala023@student.wethinkcode.co.za>
2024-10-31 11:44:28 +0200

    Exercise 4 Tests

--- Exercise4Tests/CalculatorFactory.cs
+++ Exercise4Tests/CalculatorFactory.cs
@@ -1,11 +1,15 @@
+using System.ComponentModel;
 using Calculators;
 
 namespace Exercise4Tests;
 
 public class CalculatorFactory
 {
-    public SimpleCalculator CreateWithInitialStateOf(decimal initialState)
-    {
-        return null; // replace this with your code
+    
+    public SimpleCalculator CreateWithInitialStateOf(decimal initialState){
+        var calculator = new SimpleCalculator ();
+        calculator.Enter( initialState);
+
+        return calculator ; 
     }
 }
--- Exercise4Tests/Exercise4Tests.cs
+++ Exercise4Tests/Exercise4Tests.cs
@@ -1,3 +1,4 @@
+using System.Xml.Serialization;
 using Calculators;
 
 namespace Exercise4Tests;
@@ -6,23 +7,49 @@ namespace Exercise4Tests;
 [TestFixture]
 public class Exercise4Tests
 {
+    private CalculatorFactory calculator_Factory; 
+
+    [SetUp]
+    public void Setup(){
+        // Arrange
+            calculator_Factory = new CalculatorFactory();
+    }
+
     [Test]
     public void AdditionTest()
     {
+        // Act
+        var result = calculator_Factory.CreateWithInitialStateOf(100).Plus(20).Equals();
+        // AAssert
+        Assert.That(result, Is.EqualTo(120));
     }
 
     [Test]
     public void SubtractionTest()
     {
+        // Act
+        var result = calculator_Factory.CreateWithInitialStateOf(10).Minus(5).Equals();
+        // Assert
+        Assert.That(result, Is.EqualTo(5));
     }
 
     [Test]
     public void MultiplicationTest()
     {
+        // Act 
+        var result = calculator_Factory.CreateWithInitialStateOf(25).MultiplyBy(4).Equals();
+        // Assert
+        Assert.That(result, Is.EqualTo(100));
     }
 
     [Test]
     public void DivisionTest()
     {
+        // Act
+        var result = calculator_Factory.CreateWithInitialStateOf(33).DivideBy(11).Equals();
+        // Assert
+        Assert.That(result,  Is.EqualTo(3));
+
+
     }
 }
