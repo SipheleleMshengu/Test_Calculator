5f9abf995fa9787d8f22cd3573fd5d3352fee028
sshabalala023 <sshabalala023@student.wethinkcode.co.za>
2024-10-31 11:35:20 +0200

    Exercise 2 Tests

--- Exercise2Tests/Exercise2Tests.cs
+++ Exercise2Tests/Exercise2Tests.cs
@@ -1,4 +1,5 @@
 using Calculators;
+using NUnit.Framework;
 
 namespace Exercise2Tests;
 
@@ -6,23 +7,59 @@ namespace Exercise2Tests;
 [TestFixture]
 public class Exercise1Tests
 {
+
+    private SimpleCalculator calculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Arrange 
+        calculator = new SimpleCalculator();
+        calculator.Enter(4);
+    }
+    [TearDown]
+    public void Teardown()
+    {
+        calculator.Reset();
+    }
+
     [Test]
     public void AdditionTest()
     {
+        // Act
+        var result = calculator.Plus(2).Equals();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(6));
     }
 
     [Test]
     public void SubtractionTest()
     {
+        // Act
+        var result = calculator.Minus(2).Equals();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(2));
     }
 
     [Test]
     public void MultiplicationTest()
     {
+        // Act
+        var result = calculator.MultiplyBy(2).Equals();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(8));
     }
 
     [Test]
     public void DivisionTest()
     {
+        // Act
+        var result = calculator.DivideBy(2).Equals();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(2));
     }
 }
