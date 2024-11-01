10048ee29e173855025f5df27f3cc60b12663235
SipheleleMshengu <sipheleleshabalala5@gmail.com>
2024-11-01 17:01:39 +0200

    Update CalculatorFactory.cs

--- Exercise4Tests/CalculatorFactory.cs
+++ Exercise4Tests/CalculatorFactory.cs
@@ -1,15 +1,24 @@
 using System.ComponentModel;
 using Calculators;
 
-namespace Exercise4Tests;
-
-public class CalculatorFactory
+namespace Exercise4Tests
 {
-    
-    public SimpleCalculator CreateWithInitialStateOf(decimal initialState){
-        var calculator = new SimpleCalculator ();
-        calculator.Enter( initialState);
+    /// <summary>
+    /// A factory class for creating instances of SimpleCalculator with a specified initial state.
+    /// </summary>
+    public class CalculatorFactory
+    {
+        /// <summary>
+        /// Creates an instance of SimpleCalculator and sets an initial state.
+        /// </summary>
+        /// <param name="initialState">The initial state (value) to set in the calculator.</param>
+        /// <returns>A SimpleCalculator instance with the specified initial state.</returns>
+        public SimpleCalculator CreateWithInitialStateOf(decimal initialState)
+        {
+            var calculator = new SimpleCalculator();
+            calculator.Enter(initialState);
 
-        return calculator ; 
+            return calculator;
+        }
     }
 }
