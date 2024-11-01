ace4c5cea7f8190131c231ada4d3b303c36e0c4c
SipheleleMshengu <sipheleleshabalala5@gmail.com>
2024-11-01 16:57:29 +0200

    Update BaseTest.cs , added docstrings

--- Exercise3Tests/BaseTest.cs
+++ Exercise3Tests/BaseTest.cs
@@ -1,30 +1,38 @@
 using Calculators;
 using NUnit.Framework;
 
-public class BaseTest {
+/// <summary>
+/// BaseTest class for setting up and tearing down common 
+/// resources needed for SimpleCalculator tests.
+/// </summary>
+public class BaseTest 
+{
+    /// <summary>
+    /// A protected instance of SimpleCalculator to be used by derived test classes.
+    /// </summary>
+    protected SimpleCalculator calculator { get; set; }
 
-    protected SimpleCalculator calculator { get; set;}
-
+    /// <summary>
+    /// SetUp method initializes the SimpleCalculator instance
+    /// and performs initial configuration before each test.
+    /// </summary>
     [SetUp]
     public void Setup()
     {
-        //Arrange
+        // Arrange
         calculator = new SimpleCalculator();
         calculator.Enter(4);
-        TestContext.Progress.WriteLine(" BaseTest Setup Method Invoked ! ");
+        TestContext.Progress.WriteLine("BaseTest Setup Method Invoked!");
     }
 
+    /// <summary>
+    /// TearDown method resets the SimpleCalculator instance 
+    /// after each test to ensure a clean state for the next test.
+    /// </summary>
     [TearDown]
-
     public void Teardown()
     {
         calculator.Reset();
-        TestContext.Progress.WriteLine(" BaseTest Teardown Method Invoked ! ");
-
+        TestContext.Progress.WriteLine("BaseTest Teardown Method Invoked!");
     }
-
-
-
-
-
 }
